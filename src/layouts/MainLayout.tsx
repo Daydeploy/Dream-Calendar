f956f5f4ca318914ed7019e89377a32e27bb2ee4
lars7b <larszevenbergen97@gmail.com>
2025-10-29 09:58:21 +0100

    REACT
    REACT

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet, Link } from "react-router-dom";
+
+export default function MainLayout() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet /> {/* Renders the current route’s page here */}
+      </main>
+    </div>
+  );
+}
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./Home.css";
+
+
+function Home() {
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleToggle = () => setShowRegister(!showRegister);
+
+   return (
+    <div>
+      <h1 style={{ color: "black" }}>Welcome</h1>
+      <h3 style={{ color: "black" }}>{showRegister ? "Register" : "Login"}</h3>
+
+      <div
+      style={{ width: "200px", margin: "0 auto", backgroundColor: "white", padding: "20px", borderRadius: "10px" }}
+      >
+        {!showRegister ? (
+          <form>
+            <input name="username" type="text" placeholder="Username" />
+            <input name="password" type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+            <button type="button" onClick={handleToggle}>
+              Register
+            </button>
+          </form>
+        ) : (
+          <form>
+            <input name="username" type="text" placeholder="Username" />
+            <input name="password" type="password" placeholder="Password" />
+            <input name="firstname" type="text" placeholder="First name" />
+            <input name="lastname" type="text" placeholder="Last name" />
+            <input name="email" type="email" placeholder="Email" />
+            <input name="telnumber" type="tel" placeholder="Phone number" />
+            <button className="btn" type="submit">Register</button>
+            <button className="btn" type="button" onClick={handleToggle}>
+              Back to Login
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
